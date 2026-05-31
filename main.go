17ee6cc997ccb895e2a4aaa90115ff66db3644c8
Perfected1 <chikennamadim65@gmail.com>
2026-05-31 06:10:16 -0700

    Collect subject scores using loop

--- main.go
+++ main.go
@@ -13,4 +13,16 @@ func main() {
 	fmt.Scanln(&subjectCount)
 
 	fmt.Println("You entered:", subjectCount)
+
+	// Store all subject scores here
+	scores := make([]int, subjectCount)
+
+	// Collect scores one by one
+	for i := 0; i < subjectCount; i++ {
+		fmt.Printf("Enter score for subject %d: ", i+1)
+		fmt.Scanln(&scores[i])
+	}
+
+	// Just printing what we collected for now
+	fmt.Println("Scores entered:", scores)
 }
